ee7c337894aaad683e2abdcea58032d8dd056d81
Kavya P S <kavya.ps095@gmail.com>
2024-01-04 23:07:51 +0530

    Init UI commit

--- app/src/main/java/com/kps/camerax/MainActivity.kt
+++ app/src/main/java/com/kps/camerax/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.kps.camerax
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kps.camerax.ui.theme.CameraXTheme
@@ -22,7 +28,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        PreviewView(this@MainActivity)
+                        Button(
+                            onClick = {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "clicked!",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }) {
+                            Text(text = "Take Photo!")
+                        }
+                        Button(
+                            onClick = {
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    "clicked!",
+                                     Toast.LENGTH_SHORT
+                                ).show()
+                            }) {
+                            Text(text = "Start Video Capture!")
+                        }
+                    }
                 }
             }
         }
